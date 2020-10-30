b712a66636e3205e2dc2558e605cc092af6c1dbf
Sun-2 <sun127.0.0.1@gmail.com>
2020-10-30 19:43:02 +0100

    open source dialog

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from "react";
+import React, { FC, useCallback, useMemo, useState } from "react";
 import {
   Box,
   Button,
@@ -12,6 +12,11 @@ import {
   Tooltip,
   Typography,
   Link,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
 } from "@material-ui/core";
 import styled from "styled-components";
 import LinkIcon from "@material-ui/icons/Link";
@@ -24,6 +29,7 @@ export type LayoutProps = {
   sourceUrl: string;
   sourceScreenshotUrl: string;
   sourceIconUrl: string;
+  isSourcePublic: boolean;
 
   myCodeUrl: string;
   myShowcaseUrl: string;
@@ -33,6 +39,8 @@ export type LayoutProps = {
 export const Layout: FC<LayoutProps> = (props) => {
   const { ...rest } = props;
 
+  const [showOpenSourceDialog, setShowOpenSourceDialog] = useState(false);
+
   const iconUrl = useMemo(() => {
     if (props.sourceIconUrl) return props.sourceIconUrl;
     const url = new URL(props.sourceUrl);
@@ -40,96 +48,142 @@ export const Layout: FC<LayoutProps> = (props) => {
     return url.toString();
   }, [props.sourceIconUrl || props.sourceUrl]);
 
+  const onSourceClick = useCallback(
+    (e) => {
+      if (props.isSourcePublic) return window.open(props.sourceUrl, "_blank");
+      setShowOpenSourceDialog(true);
+    },
+    [props.isSourcePublic]
+  );
+
+  const closeSourceDialog = useCallback(
+    () => setShowOpenSourceDialog(false),
+    []
+  );
+
   return (
-    <Grid item xs={12} xl={6}>
-      <RootBox component={Paper} pt={3} pb={3} iconUrl={props.sourceIconUrl}>
-        <Box display={"flex"} justifyContent={"center"}>
-          <Box
-            display={"inline-flex"}
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Box display="flex" alignItems={"center"}>
-              <img height={16} src={props.sourceIconUrl} alt="" />
-              <Box pl={1}>
-                <Link href={props.sourceUrl}>
-                  <Typography align="center" variant="h6">
-                    {props.sourceName}
-                  </Typography>
-                </Link>
+    <>
+      <Dialog {...rest} open={showOpenSourceDialog}>
+        <DialogTitle>Private source</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            The source of this layout is unavailable, probably because you must
+            be logged in on the <a target="_blank" href={props.sourceUrl}>target site</a> to see
+            it.
+          </DialogContentText>
+          <DialogActions>
+            <Button onClick={closeSourceDialog}>Cancel</Button>
+            <Button href={props.sourceScreenshotUrl}>View screenshot</Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+      <Grid item xs={12} xl={6}>
+        <RootBox component={Paper} pt={3} pb={3} iconUrl={iconUrl}>
+          <Box display={"flex"} justifyContent={"center"}>
+            <Box
+              display={"inline-flex"}
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Box display="flex" alignItems={"center"}>
+                <img height={16} src={iconUrl} alt="" />
+                <Box pl={1}>
+                  <Link href={props.sourceUrl}>
+                    <Typography align="center" variant="h6">
+                      {props.sourceName}
+                    </Typography>
+                  </Link>
+                </Box>
               </Box>
+
+              <Divider
+                flexItem
+                orientation="horizontal"
+                style={{ height: "1px" }}
+              />
+
+              <Typography align="center" variant={"h6"}>
+                {props.layoutName}
+              </Typography>
             </Box>
-
-            <Divider
-              flexItem
-              orientation="horizontal"
-              style={{ height: "1px" }}
-            />
-
-            <Typography align="center" variant={"h6"}>
-              {props.layoutName}
-            </Typography>
           </Box>
-        </Box>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          p={2}
-          width={"50%"}
-          mx="auto"
-        >
-          <LinksButtonGroup variant="text" fullWidth>
-            <LinkButton href={props.myShowcaseUrl} size="small">
-              View Live
-            </LinkButton>
-            <LinkButton href={props.myCodeUrl} size="small">
-              My Code
-            </LinkButton>
-            <LinkButton href={props.sourceUrl} size="small">
-              Source
-            </LinkButton>
-          </LinksButtonGroup>
-        </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            p={2}
+            width={"50%"}
+            mx="auto"
+          >
+            <LinksButtonGroup variant="text" fullWidth>
+              <LinkButton
+                href={props.myShowcaseUrl}
+                size="small"
+                //@ts-ignore
+                target={"_blank"}
+              >
+                View Live
+              </LinkButton>
+              <LinkButton
+                href={props.myCodeUrl}
+                size="small"
+                //@ts-ignore
+                target={"_blank"}
+              >
+                My Code
+              </LinkButton>
+              <LinkButton
+                onClick={onSourceClick}
+                size="small"
+                //@ts-ignore
+                target={"_blank"}
+              >
+                Source
+              </LinkButton>
+            </LinksButtonGroup>
+          </Box>
 
-        <Grid container>
-          <ImgGrid item xs={12} md={6}>
-            <Box
-              component="a"
-              /* @ts-ignore */
-              href={props.myScreenshotUrl}
-            >
-              <Tooltip title="Replica" placement="top">
-                <img
-                  height={"100%"}
-                  width={"100%"}
-                  src={props.myScreenshotUrl}
-                />
-              </Tooltip>
-            </Box>
-          </ImgGrid>
+          <Grid container>
+            <ImgGrid item xs={12} md={6}>
+              <Box
+                component="a"
+                /* @ts-ignore */
+                href={props.myScreenshotUrl}
+              >
+                <Tooltip title="Replica" placement="top">
+                  <a onClick={onSourceClick} target={"_blank"}>
+                    <img
+                      height={"100%"}
+                      width={"100%"}
+                      src={props.myScreenshotUrl}
+                    />
+                  </a>
+                </Tooltip>
+              </Box>
+            </ImgGrid>
 
-          <ImgGrid item xs={12} md={6}>
-            <Box
-              component="a"
-              /* @ts-ignore */
-              href={props.sourceUrl}
-            >
-              <Tooltip title="source" placement="top">
-                <img
-                  height={"100%"}
-                  width={"100%"}
-                  src={props.sourceScreenshotUrl}
-                />
-              </Tooltip>
-            </Box>
-          </ImgGrid>
-        </Grid>
-      </RootBox>
-      <DividerBox>
-        <Divider />
-      </DividerBox>
-    </Grid>
+            <ImgGrid item xs={12} md={6}>
+              <Box
+                component="a"
+                /* @ts-ignore */
+                href={props.sourceUrl}
+              >
+                <Tooltip title="source" placement="top">
+                  <img
+                    height={"100%"}
+                    width={"100%"}
+                    src={props.sourceScreenshotUrl}
+                  />
+                </Tooltip>
+              </Box>
+            </ImgGrid>
+          </Grid>
+        </RootBox>
+        <DividerBox>
+          <Divider />
+        </DividerBox>
+      </Grid>
+    </>
   );
 };
 
